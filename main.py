456f29ceae7cff1140a8151bcc4d63187df9af0d
wolflergf <wolflergf@gmail.com>
2023-11-12 12:52:21 +0000

    fixed the lines

--- main.py
+++ main.py
@@ -12,7 +12,8 @@ for index, row in df.iterrows():
     pdf.set_font(family="Times", style="B", size=24)
     pdf.set_text_color(100, 100, 100)
     pdf.cell(w=0, h=12, txt=row["Topic"], align="L", ln=1)
-    pdf.line(10, 21, 200, 21)
+    for i in range(20, 298, 10):
+        pdf.line(10, i, 200, i)
 
     # Set the footer
     pdf.ln(265)
@@ -28,6 +29,9 @@ for index, row in df.iterrows():
         pdf.set_font(family="Times", style="I", size=8)
         pdf.set_text_color(180, 180, 180)
         pdf.cell(w=0, h=10, txt=row["Topic"], align="R")
+        pdf.line(10, 21, 200, 21)
+        for i in range(20, 298, 10):
+            pdf.line(10, i, 200, i)
 
 
 pdf.output("output.pdf")
